d4646ed1bdfd3bcc5f64efe473855de4eb990bd0
Martin Eesmaa <martin.eesmaa@protonmail.com>
2023-05-10 16:44:41 +1000

    Fix Business door teleportation of next to scene

--- Assets/Scripts/BusinessDoor.cs
+++ Assets/Scripts/BusinessDoor.cs
@@ -5,9 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class BusinessDoor : MonoBehaviour
 {
+    public int LevelIndex;
     void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Player")
-        SceneManager.LoadScene("Business");
+        {
+            SceneManager.LoadScene(LevelIndex);
+        }
     }
 }
