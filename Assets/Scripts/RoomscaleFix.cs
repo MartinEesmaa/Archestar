d696e98ace868825f681dd0e2b6d7cd9b9dcd8f2
Martin Eesmaa <martin.eesmaa@protonmail.com>
2023-05-20 18:44:51 +1000

    Change move action to MovePlayer and fix roomscale

--- Assets/Scripts/RoomscaleFix.cs
+++ Assets/Scripts/RoomscaleFix.cs
@@ -1,24 +1,35 @@
-using Unity.XR.CoreUtils;
 using UnityEngine;
-
-[RequireComponent(typeof(CharacterController))]
-[RequireComponent(typeof(XROrigin))]
+using Valve.VR;
 
 public class RoomscaleFix : MonoBehaviour
 {
-    CharacterController _character;
-    XROrigin _xrOrigin;
+    public float playAreaHeight = 2f;
+    public float playAreaWidth = 2f;
+
+    private CharacterController _character;
+
     void Start()
     {
         _character = GetComponent<CharacterController>();
-        _xrOrigin = GetComponent<XROrigin>();
+        if (_character == null)
+        {
+            _character = gameObject.AddComponent<CharacterController>();
+        }
     }
 
     void FixedUpdate()
     {
-        var centerPoint = transform.InverseTransformPoint(_xrOrigin.Camera.transform.position);
-        _character.center = new Vector3(centerPoint.x, _character.height / 2 + _character.skinWidth, centerPoint.z);
-        _character.Move(new Vector3(0.001f, -0.001f, 0.001f));
-        _character.Move(new Vector3(-0.001f, -0.001f, -0.001f));
+        var playArea = new HmdQuad_t();
+        SteamVR_PlayArea.GetBounds(SteamVR_PlayArea.Size.Calibrated, ref playArea);
+        var centerPoint = SteamVR_Render.Top().head.position;
+        _character.center = new Vector3(centerPoint.x - transform.position.x, playAreaHeight / 2 + _character.skinWidth, centerPoint.z - transform.position.z);
+        _character.height = playAreaHeight;
+        _character.radius = Mathf.Min(playArea.vCorners0.v0 - playArea.vCorners2.v0, playArea.vCorners2.v2 - playArea.vCorners0.v2) / 2f;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(transform.position + _character.center, new Vector3(playAreaWidth, playAreaHeight, _character.radius * 2f));
     }
 }
